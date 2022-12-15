cf874c405bd8d52d387adce1447ce56212a0f625
Jasper Sival <jasper.sival@alliander.com>
2022-12-15 09:12:43 +0100

    Time fibonacci using memoization with both python and rust

--- src/fibonacci.py
+++
@@ -1,7 +0,0 @@
-def print_fibonacci(first, second, length):
-    if length == 0:
-        return
-
-    print(first + second, end=" ")
-
-    print_fibonacci(second, first+second, length-1)
--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,41 @@
 #![allow(unused)]
 fn main() {
 use pyo3::prelude::*;
+use std::collections::HashMap;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn fibonacci(n: u64, memo: &mut HashMap<u64, u64>) -> u64 {
+    match n {
+        0 | 1 => n,
+        _ => {
+            if let Some(&fib_n) = memo.get(&n) {
+                fib_n
+            } else {
+                let fib_n = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+                memo.insert(n, fib_n);
+                fib_n
+            }
+        }
+    }
 }
+
+#[pyfunction]
+fn rust_fibonacci(n: u64) -> PyResult<u64>{
+    let mut memo = HashMap::new();
+    return Ok(fibonacci(n, &mut memo));
+}
+// fn fibonacci(n: u64) -> u64 {
+//     if n <= 1 {
+//         return n;
+//     }
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+// }
 
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
-fn string_sum(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn alliander_rust_functions(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(rust_fibonacci, m)?)?;
 
     Ok(())
 }
