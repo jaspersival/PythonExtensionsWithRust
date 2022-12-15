a168191094920398130ee01b08dbb96d98121ed7
Jasper Sival <jasper.sival@alliander.com>
2022-12-15 17:12:36 +0100

    Split calculate_factorial from main in main.rs and add unit tests for calculate_factorial

--- src/factorial/src/main.rs
+++ src/factorial/src/main.rs
@@ -1,14 +1,48 @@
 mod cli;
 use cli::parse_args;
 
-fn main() {
-    let args = parse_args();
-    let mut n: u64 = args.number;
-    let original_n = n;
+fn calculate_factorial(mut n: u64) -> u64 {
     let mut factorial: u64 = 1;
     while n > 0 {
         factorial *= n;
         n -= 1;
     }
+    factorial
+}
+fn main() {
+    let args = parse_args();
+    let n: u64 = args.number;
+    let original_n = n;
+    let factorial = calculate_factorial(n);
     println!("The factorial of {} is {}", original_n, factorial)
+}
+
+#[test]
+fn calculate_factorial_with_5_returns_120() {
+    let number: u64 = 5;
+    let expected: u64 = 120;
+
+    let result = calculate_factorial(number);
+
+    assert_eq!(result, expected)
+}
+
+#[test]
+fn calculate_factorial_with_0_returns_1() {
+    let number: u64 = 0;
+    let expected: u64 = 1;
+
+    let result = calculate_factorial(number);
+
+    assert_eq!(result, expected)
+}
+
+#[test]
+fn calculate_factorial_with_1_returns_1() {
+    let number: u64 = 1;
+    let expected: u64 = 1;
+
+    let result = calculate_factorial(number);
+
+    assert_eq!(result, expected)
 }
